42b1ccf27441ac8df6b98f16e85032489daf96f4
pRob3 <hundraprocent@me.com>
2020-08-28 13:09:36 +0200

    Created data classes and DTOs. Included AutoMapper.

--- BookStore-API/Startup.cs
+++ BookStore-API/Startup.cs
@@ -17,6 +17,8 @@ using System.Reflection;
 using System.IO;
 using BookStore_API.Contracts;
 using BookStore_API.Services;
+using AutoMapper;
+using BookStore_API.Mappings;
 
 namespace BookStore_API
 {
@@ -46,6 +48,9 @@ namespace BookStore_API
                     .AllowAnyMethod()
                     .AllowAnyHeader());
             });
+
+            // AutoMapper
+            services.AddAutoMapper(typeof(Maps));
 
             // Swagger
             services.AddSwaggerGen(c => {
